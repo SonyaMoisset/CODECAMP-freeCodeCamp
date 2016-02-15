bcc88cf43369f25b00c72d3f0be8125e557f9360
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-15 23:49:52 +0900

    Remove solution

--- Basic-Algorithm-Scripting/Factorialize-a-Number/solution/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function factorialize(num) {
-  // if num is strictly equals to 1, the factorial will return 1
-  if (num === 0)
-    return 1;
-
-  // else apply the factorial method
-  return num * factorialize(num - 1);
-}
